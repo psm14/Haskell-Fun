9c6985e8422f4cc425d10fc3718590eac6d7db93
Pat McLaughlin <pat.mclaughlin@mobiledefense.com>
2013-07-18 19:42:15 -0400

    Brainfuck interpreter (probably) working

--- bf.hs
+++ bf.hs
@@ -1,4 +1,5 @@
 import Control.Monad.State
+import Data.Char
 
 data Mem = Mem [Int] Int [Int]
          deriving (Show)
@@ -29,8 +30,45 @@ memDec = memMod $ flip (-) 1 -- Parser reads (-1) as negative 1
 memSet :: Int -> Mem -> Mem
 memSet b = memMod $ const b
 
-bfStep :: Char -> StateT ([String],Mem) IO ()
-bfStep = undefined
+memOp :: (Mem -> Mem) -> String -> StateT ([String],Mem) IO ()
+memOp f cs = do (stack,mem) <- get
+                put (stack, f mem)
+                bfStep cs
+
+findJump' :: Int -> String -> String
+findJump' 0 (']' : cs) = cs
+findJump' n (']' : cs) = findJump' (n - 1) cs
+findJump' n ('[' : cs) = findJump' (n + 1) cs
+findJump' n (c : cs)   = findJump' n cs
+findJump' n "" = error "Jumped off the end of the program"
+
+findJump :: String -> String
+findJump cs = findJump' 0 cs
+    
+bfStep :: String -> StateT ([String],Mem) IO ()
+bfStep "" = lift $ return ()
+bfStep ('<' : cs) = memOp memLeft  cs
+bfStep ('>' : cs) = memOp memRight cs
+bfStep ('+' : cs) = memOp memInc   cs
+bfStep ('-' : cs) = memOp memDec   cs
+bfStep ('.' : cs) = do (stack,mem) <- get
+                       lift $ putChar (chr $ memDeref mem)
+                       bfStep cs
+bfStep (',' : cs) = do (stack,mem) <- get
+                       c <- lift $ getChar
+                       put (stack, memSet (ord c) mem)
+                       bfStep cs
+bfStep ('[' : cs) = do (stack, mem) <- get
+                       if memDeref mem == 0 then
+                         bfStep $ findJump cs
+                       else
+                         put (cs : stack, mem) >> bfStep cs
+bfStep (']' : cs) = do (stack, mem) <- get
+                       if memDeref mem == 0 then
+                         put (tail stack,mem) >> bfStep cs
+                       else
+                         bfStep $ head stack
+bfStep (c : cs) = bfStep cs
 
 brainfuck :: String -> IO ()
-brainfuck = undefined
+brainfuck bf = evalStateT (bfStep bf) ([],blankMem)
